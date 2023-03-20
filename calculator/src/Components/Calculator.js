bb4c0db92b86e76b9a8704a0d412776be96539d1
Claire McGuigan <mcguigan.claire@outlook.com>
2023-03-19 20:32:29 -0400

    Initializing React app and basic set up

---
+++ calculator/src/Components/Calculator.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import './Calculator.css';
+
+export default function Calculator(){
+
+    return (
+        <div id="main-container">
+            <h1>Hi</h1>
+        </div>
+    );
+}
